db361754adb858eca15a3a9945a4fad98946049d
Zhaoji Wang <hwoam@outlook.com>
2019-08-10 06:58:19 +0800

    feat(TrainingScheme): 添加根据id获取数据的功能

--- application/student/controller/TrainingScheme.php
+++ application/student/controller/TrainingScheme.php
@@ -4,9 +4,31 @@ namespace app\student\controller;
 
 class TrainingScheme
 {
+  private $trainingSchemeListString;
+  private $trainingSchemeList;
+
+  function __construct()
+  {
+    $this->trainingSchemeListString = file_get_contents('training-scheme-list.json', true);
+  }
+
   public function index()
   {
-    $json_string = file_get_contents('training-scheme-list.json', true);
-    return response($json_string)->header(['Content-Type' => 'application/json; charset=utf-8']);
+    return response($this->trainingSchemeListString)->header(['Content-Type' => 'application/json; charset=utf-8']);
+  }
+
+  public function read($id)
+  {
+    $this->trainingSchemeList = json_decode($this->trainingSchemeListString);
+    $result = [];
+    foreach ($this->trainingSchemeList as $value) {
+      $result[$value[0]] = $value;
+    }
+    return json([
+      'error' => 0,
+      'data' => [
+        'trainingScmeme' => $result[$id]
+      ]
+    ]);
   }
 }
