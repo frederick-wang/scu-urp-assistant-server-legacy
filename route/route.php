7917074312284ee3acd013f5a3b1995404caadb6
Zhaoji Wang <hwoam@outlook.com>
2019-08-11 11:27:08 +0800

    fix(router): 删除了任意跨域的header

--- route/route.php
+++ route/route.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 
 Route::resource('student/training_scheme', 'student/TrainingScheme');
-Route::resource('student/course_score_infos', 'student/StudentCourseScoreInfos')->header('Access-Control-Allow-Origin', '*');
+Route::resource('student/course_score_infos', 'student/StudentCourseScoreInfos');
 Route::resource('course/course_score_info', 'course/CourseScoreInfo');
 Route::resource('course/course_score_infos', 'course/CourseScoreInfos');
 
